20bdcdc1e1c9385e47ec24b877ee94ddad7d9196
berenar <bernatpericasserra97@gmail.com>
2019-05-21 12:59:32 +0200

    afegits arrays i funcions: normalitza(), conte()

--- web/voice/bernat.js
+++ web/voice/bernat.js
@@ -1,30 +1,58 @@
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 const synth = window.speechSynthesis;
 const recognition = new SpeechRecognition();
-
-const icon = document.querySelector('i.fa.fa-microphone')
+let rec;
+const icon = document.querySelector('i.fa.fa-microphone');
 const sound = document.querySelector('.sound');
+//const myVideo = document.getElementById('mainVid');
 
+const all_play = ['play', 'reproducir', 'reproduce', 'reprodueix'];
+const all_pause = ['pause', 'pausar', 'pausa'];
+const all_stop = ['stop', 'detener', 'deten', 'detenir'];
+const all_ENG = ['english', 'ingles', 'angles'];
+const all_PT = ['portuguese', 'portugal', 'portugues'];
+const all_CH = ['chinese', 'chino', 'xines'];
+const all_ESP = ['español', 'spanish', 'espanyol', 'castella', 'castellano'];
+
+
+/*Listener of the mic icon that triggers the sound and the text recognition function*/
 icon.addEventListener('click', () => {
     sound.play();
     dictate();
 });
 
-//TODO: fer que sigui case insensitive
+/* Remove accents and special characters and to lower case */
+function normalitza(rec) {
+    return rec.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+}
 
+/* Searches if the recognized text is included in a given array of Strings */
+function conte(all_play) {
+    for (let i = 0; i < all_play.size(); i++) {
+        if (normalitza(rec).includes(all_play[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/* Checks if the recognized text coincides with any of the declared arrays and, if so, executes the matching actions */
 const dictate = () => {
     recognition.start();
     recognition.onresult = (event) => {
-        const speechToText = event.results[0][0].transcript;
-        console.log(speechToText);
+        rec = event.results[0][0].transcript;
+        console.log(rec);
 
         if (event.results[0].isFinal) {
 
-            if (speechToText.includes('Reproducir')) {
-                document.getElementById('mainVid').play();
-            } else if (speechToText.includes('pausar')) {
-                document.getElementById('mainVid').pause();
+            if (conte(all_play)) {
+                myVideo.play();
+            } else if (conte(all_pause)) {
+                myVideo.pause();
+            } else if (conte(all_stop)) {
+                myVideo.stop();
             }
         }
     }
 }
+
